fc7a0ae98d0aa6a4340a75cb71971bb52c107e1c
Michael Bruty <57625243+mbruty@users.noreply.github.com>
2022-01-12 16:34:30 +0000

    Added guild name resolver to player query (#7)
    * Added guild name resolver to player query
    
    * [ci skip] Bump version number
    
    Co-authored-by: mbruty <mbruty@users.noreply.github.com>

--- src/main/kotlin/io/guildtools/swgraphql/cache/GuildRepository.kt
+++ src/main/kotlin/io/guildtools/swgraphql/cache/GuildRepository.kt
@@ -1,8 +1,15 @@
 package io.guildtools.swgraphql.cache
 
 import io.guildtools.swgraphql.model.types.Guild
+import io.guildtools.swgraphql.model.types.Player
+import org.springframework.data.mongodb.repository.Aggregation
 import org.springframework.data.mongodb.repository.MongoRepository
 
 interface GuildRepository: MongoRepository<Guild, String> {
     fun findByRosterAllyCode(allyCode: Int): Guild?
+    @Aggregation(
+        "{\'\$match\': { '_id': '?0' } }",
+        "{\"\$project\": {\"name\": 1 } }"
+    )
+    fun findNameByGuildRef(guildRef: String): Guild?
 }
