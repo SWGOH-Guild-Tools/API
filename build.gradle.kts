72e736be4af920e3571d3e06beee45cc27ba4231
Michael Bruty <57625243+mbruty@users.noreply.github.com>
2022-01-13 20:08:27 +0000

    Ci optimise (#9)
    * Moving release to github packages
    
    * Paralellising workflows that can be

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ plugins {
 }
 
 group = "io.guildtools"
-version = "0.5.49"
+version = "0.5.56-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
--- dockerfile
+++ dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:17-alpine
 ARG JAR_NAME
-COPY /build/libs/$JAR_NAME /app.jar
+COPY $JAR_NAME /app.jar
 EXPOSE 8080
 ENTRYPOINT ["java","-jar","/app.jar"]
